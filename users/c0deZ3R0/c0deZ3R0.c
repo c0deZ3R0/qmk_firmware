bb2cf8f7e3e4af76c0e58bcbe660494e74b5457d
c0deZ3R0 <blaine_wordly@hotmail.com>
2023-03-03 00:06:16 +1100

    Userspace Setup

---
+++ users/c0deZ3R0/c0deZ3R0.c
@@ -0,0 +1,6 @@
+// Copyright 2023 Blaine Wordly, aka c0deZ3R0  <blaine_wordly@hotmail.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "c0deZ3R0.h"
+
+userspace_config_t userspace_config;
